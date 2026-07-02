ce9568c84a5aa98366f2270941e9d1924f3ca82d
Skywalker93-hub <nothern4lights@gmail.com>
2026-07-02 14:03:51 +0300

    Added the script for processing arithmetic operations with float

---
+++ arithmetic-operations.py
@@ -0,0 +1,8 @@
+a = float(input('Write the float: ')) # input a float value for variable a here
+b = float(input('Write the float: ')) # input a float value for variable b here
+
+print(a + b) # output the result of addition here
+print(a - b) # output the result of subtraction here
+print(a * b) # output the result of multiplication here
+print(a / b) # output the result of division here
+print("\nThat's all, folks!")
