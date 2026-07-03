30bec250b0042740f54652b46e13701bc50e2d39
Skywalker93-hub <nothern4lights@gmail.com>
2026-07-03 20:43:12 +0300

    Added the script for a for loop that counts to 5 with a one-second delay between each number, followed by a message. The previous code related to user input and flower names has been removed.

---
+++ if_elif_else.py
@@ -0,0 +1,10 @@
+x = input("Enter the name of the most beautiful flower: ")
+
+if x == "spathiphyllum":
+   print("No, I want a big Spathiphyllum!")
+elif x == "pelargonium":
+   print ("Spathiphyllum! Not pelargonium!")
+elif x == "Spathiphyllum":
+    print ("Yes - Spathiphyllum is the best plant ever!")
+else:
+    print("I do not know what that is.")
