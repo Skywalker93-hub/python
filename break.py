d1b99dc22e18accc54675de0b96da00573cf3f00
Skywalker93-hub <nothern4lights@gmail.com>
2026-07-06 16:09:22 +0300

    Deleted the old name

--- break.py
+++
@@ -1,8 +0,0 @@
-secret_word = "chupacabra"
-
-while True:
-    input_word = input("Enter the secret word: ")
-    if input_word == secret_word:
-        print("You've successfully left the loop.")
-        break
-        
